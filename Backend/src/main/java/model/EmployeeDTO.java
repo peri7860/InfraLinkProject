c0b73d8b9193381981e031622c67f5dd1f215db3
SEOKWON <presium575730@gmail.com>
2026-09-02 14:52:29 +0900

    EmployeeDTO

---
+++ Backend/src/main/java/model/EmployeeDTO.java
@@ -0,0 +1,87 @@
+package model;
+
+public class EmployeeDTO {
+
+	private String employee_id;
+	private String password;
+	private String emp_name;
+	private String dept_name;
+	private String position;
+	private String email;
+	private String ext_no;
+	private String hire_date;
+	private String emp_status;
+
+	public String getEmployee_id() {
+		return employee_id;
+	}
+
+	public void setEmployee_id(String employee_id) {
+		this.employee_id = employee_id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmp_name() {
+		return emp_name;
+	}
+
+	public void setEmp_name(String emp_name) {
+		this.emp_name = emp_name;
+	}
+
+	public String getDept_name() {
+		return dept_name;
+	}
+
+	public void setDept_name(String dept_name) {
+		this.dept_name = dept_name;
+	}
+
+	public String getPosition() {
+		return position;
+	}
+
+	public void setPosition(String position) {
+		this.position = position;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getExt_no() {
+		return ext_no;
+	}
+
+	public void setExt_no(String ext_no) {
+		this.ext_no = ext_no;
+	}
+
+	public String getHire_date() {
+		return hire_date;
+	}
+
+	public void setHire_date(String hire_date) {
+		this.hire_date = hire_date;
+	}
+
+	public String getEmp_status() {
+		return emp_status;
+	}
+
+	public void setEmp_status(String emp_status) {
+		this.emp_status = emp_status;
+	}
+
+}
